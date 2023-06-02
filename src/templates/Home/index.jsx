23c2b4d6fd359b8607b7876cf3fb8a84cd91d246
João Victor Sá de Araújo <112119158+Joaovsaa@users.noreply.github.com>
2023-06-02 14:57:03 -0300

    BuildActions & Reducer

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useCounterContext } from '../../contexts/CounterContext';
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
+
 export const Home = () => {
+  const [state, actions] = useCounterContext();
+
+
+  const handleError = () => {
+    actions.asyncError().then((r) => console.log(r)).catch((e) => console.log(e.name, ':', e.message));
+  };
+
+  useEffect(() => {
+    actions.increase();
+  }, [actions]);
 
   return (
     <div>
-     <h1>Oi</h1>
+      <Heading />
+      <div>
+        <Button onButtonClick={() => actions.increase()}>Increase</Button>
+        <Button onButtonClick={() => actions.decrease()}>Decrease</Button>
+        <Button onButtonClick={() => actions.reset()}>reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>Set 10</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>Set 100</Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>Async Increase</Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>Async Error</Button>
+      </div>
+
+
+
     </div>
   );
 };
